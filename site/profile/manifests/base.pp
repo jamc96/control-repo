9d8c3b1e8bb83882bacb4a31d6b72e13ccd43948
Jose Mejia <jamc.mejia@gmail.com>
2018-09-12 22:20:01 +0530

    adding validation for env

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,7 +1,13 @@
 class profile::base {
   # modules
+  # test emvironment
+  $servers = $facts['env'] ? {
+    'production' => ['torntp.sandals.com','vanntp.sandals.com'],
+    default => ['vanntp.sandals.com','torntp.sandals.com'],
+  }
+  # add class
   class { '::ntp':
-    servers => ['torntp.sandals.com','vanntp.sandals.com'],
+    servers => $servers,
   }
 }
 
