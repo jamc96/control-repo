0f7396b9119f60dba099d3512c0e0dc5f70f6a1a
Jose Mejia <jamc.mejia@gmail.com>
2018-09-12 22:28:31 +0530

    adding ntp defaults

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,13 +1,5 @@
 class profile::base {
   # modules
-  # test emvironment
-  $servers = $facts['env'] ? {
-    'production' => ['torntp.sandals.com','vanntp.sandals.com'],
-    default => ['vanntp.sandals.com','torntp.sandals.com'],
-  }
-  # add class
-  class { '::ntp':
-    servers => $servers,
-  }
+  include ::ntp
 }
 
